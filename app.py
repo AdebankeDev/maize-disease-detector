5a79a4cea232ae6b997a6783808efa705383277c
AdebankeDev <adebankepeke04@gmail.com>
2026-06-02 06:54:30 +0100

    Maize_Disease_Classification

--- app.py
+++ app.py
@@ -150,7 +150,11 @@ if uploaded_file is not None:
 
     with col1:
         st.subheader("📷 Image")
-        st.image(image, use_container_width=True)
+        import streamlit as st
+        try:
+            st.image(image, use_container_width=True)
+        except TypeError:
+            st.image(image, use_column_width=True)
 
     with col2:
         st.subheader("📊 Prediction")
