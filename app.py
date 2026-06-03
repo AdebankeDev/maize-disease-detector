902ca98c422584be8060634d96021571787c1eec
Adebanke Eunice Peke <113606888+AdebankeDev@users.noreply.github.com>
2026-06-03 10:58:09 +0100

    Fix file uploader type declaration syntax

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ Technique: Transfer Learning
 
 uploaded_file = st.file_uploader(
     "📷 Select Leaf Image",
-    type=["jpg", "jpeg", "png"]
+    type=["jpg", "jpeg", "png"],
     help="Supported formats: JPG, JPEG, PNG"
 )
 
